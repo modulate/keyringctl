783ea6a194ffe47fc1100a2788473e67ca3d533d
Jared Hanson <jaredhanson@gmail.com>
2016-05-29 13:51:36 -0700

    Add vault.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,19 @@
-exports = module.exports = function(Crypto) {
+exports = module.exports = function(keyring, Crypto) {
   
   return function run() {
     Crypto.generateKey({ type: 'RSA', bits: 1024 }, function(err, key) {
+      if (err) { throw err; }
       
+      keyring.add(key, function(err) {
+        console.log('ADDED!');
+        console.log(err);
+        
+      });
     });
   }
 }
 
-exports['@require'] = [ 'http://i.bixbyjs.org/crypto' ];
+exports['@require'] = [
+  'crypto/db/vault/keyring', //'http://i.bixbyjs.org/crypto/Keyring',
+  'http://i.bixbyjs.org/crypto'
+];
